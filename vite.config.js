7865a2eb0504ba2f52d2481e30ded9a9617f9cd4
suyttthideptrai <ntienthanhn@gmail.com>
2024-05-16 22:02:38 +0700

    update 16-5 2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/baby_front/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
